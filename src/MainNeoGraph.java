e89b8bd7f3bcc651abe67a98e80a16e13f663371
pandeajay <pandeaj@gmail.com>
2014-06-20 02:54:29 -0700

    6

--- src/MainNeoGraph.java
+++ src/MainNeoGraph.java
@@ -1,7 +1,3 @@
-import graphs.Graph
-;
-import graphs.impl.NeoGraph;
-
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -10,10 +6,10 @@ import business.impl.GraphBuilderImpl;
 import business.impl.GraphQueryImpl;
 import utilities.Utils;
 
-
 public class MainNeoGraph {
 
 	String nodeSeparator = "-";
+
 	public static void main(String[] args) {
 		new MainNeoGraph().start();
 
@@ -21,48 +17,55 @@ public class MainNeoGraph {
 
 	private void start() {
 		try {
-			GraphBuilderImpl graphBuilder = new GraphBuilderImpl();					
+
+			GraphBuilderImpl graphBuilder = new GraphBuilderImpl();
 			graphBuilder.initialize();
 			graphBuilder.buildGraph();
-			
+
 			GraphQueryImpl query = new GraphQueryImpl();
 			query.initialize(graphBuilder.getGraph());
-			
-			Map<String, String> userInputMap = Utils.getDataNodesFile();			
+
+			Map<String, String> userInputMap = Utils.getDataNodesFile();
 			List<String> queryList = Utils.getFromAndToList(userInputMap);
-			
-			
-			
-			for(String str : queryList){
+
+			for (String str : queryList) {
 				String[] strs = str.split("-");
-				if(strs.length == 2){
-					
-					double weight = query.getShortestPathWeight(strs[0], strs[1]);				
-					List<?> edgeList = query.getShortestPathVetices(strs[0], strs[1]);				
-					Iterator<?> it = edgeList.iterator();				
+				if (strs.length == 2) {
+
+					double weight = query.getShortestPathWeight(strs[0],
+							strs[1]);
+					List<?> edgeList = query.getShortestPathVetices(strs[0],
+							strs[1]);
+					Iterator<?> it = edgeList.iterator();
 					String shortestPath = "";
-					while(it.hasNext()){
+					while (it.hasNext()) {
 						Object edge = it.next();
-						shortestPath += edge.toString() + nodeSeparator;					
+						shortestPath += edge.toString() + nodeSeparator;
 					}
-					if(shortestPath != null && shortestPath.length() > 0  ){
-						shortestPath = shortestPath.substring(0,shortestPath.length() - 1);
+					if (shortestPath != null && shortestPath.length() > 0) {
+						shortestPath = shortestPath.substring(0,
+								shortestPath.length() - 1);
 					}
-					
-					System.out.println("For fromtoPair = " + strs[0]+"--" + strs[1] +
-							"     shortestPath = " + (shortestPath.length() == 0 ? "Path does not exist"  : shortestPath)+ 
-							" and its weight = " +weight+ (weight > 999 ? "   practically impossible" : "") );
+
+					System.out
+							.println("For fromtoPair = "
+									+ strs[0]
+									+ "--"
+									+ strs[1]
+									+ "     shortestPath = "
+									+ (shortestPath.length() == 0 ? "Path does not exist"
+											: shortestPath)
+									+ " and its weight = "
+									+ weight
+									+ (weight > 999 ? "   practically impossible"
+											: ""));
 				}
-			}				
-			
+			}
+
 			graphBuilder.close();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}	
+		}
 	}
-	
-	
-	
 
 }
---
+++ src/utilities/Logger.java
@@ -0,0 +1,31 @@
+package utilities;
+
+public class Logger {
+	private static java.util.logging.Logger logWriter = null;
+	
+	static void init(java.util.logging.Logger logr){
+		if(logr == null){
+			logWriter = java.util.logging.Logger.getLogger(Logger.class.getName());
+		}else{
+			Logger.logWriter = logr;
+		}
+	}
+	static void fine(String msg){		
+		Logger.logWriter.fine(msg);
+	}	
+	static void info(String msg){
+		Logger.logWriter.info(msg);
+	}
+	static void config(String msg){
+		Logger.logWriter.config(msg);
+	}
+	static void finer(String msg){
+		Logger.logWriter.finer(msg);
+	}
+	static void finest(String msg){
+		Logger.logWriter.finest(msg);
+	}
+	static void warning(String msg){
+		Logger.logWriter.warning(msg);
+	}
+}
